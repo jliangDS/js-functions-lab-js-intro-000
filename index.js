18f83d08cec14609aead69f0171d7f0c60c26fa6
Jun Liang <illuminatemynight@gmail.com>
2019-06-16 20:18:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidaysTo(name, holiday){
   return `Happy ${holiday}, ${name}!`
 }
